388e0c11bc14a5e2744f79f46cd4f243e0db5af8
blikblum <blikblum@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2007-03-15 13:12:20 +0000

    Added dummy MMSystem unit to linux/gtk
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@127 8e941d3f-bd1b-0410-a28a-d453659cc2b4

---
+++ components/virtualtreeview-unstable/units/gtk/mmsystem.pas
@@ -0,0 +1,34 @@
+unit mmsystem;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Types;
+  
+function timeBeginPeriod(x1: DWord): DWord;
+
+function timeEndPeriod(x1: DWord): DWord;
+
+function timeGetTime: DWORD;
+
+implementation
+
+function timeBeginPeriod(x1: DWord): DWord;
+begin
+
+end;
+
+function timeEndPeriod(x1: DWord): DWord;
+begin
+
+end;
+
+function timeGetTime: DWORD;
+begin
+
+end;
+
+end.
+
