f58dd456cd6c8f284726f7d35ecb2e90ec1a8a9c
sekelsenmat <sekelsenmat@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2008-03-02 02:17:37 +0000

    Adds the capability to add subviews and adds initial NSTextField and NSStatusBar / NSStatusItem.
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@364 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- bindings/pascocoa/examples/simplewindow/simplewindow.pas
+++ bindings/pascocoa/examples/simplewindow/simplewindow.pas
@@ -21,24 +21,26 @@ uses
   objc, ctypes, FPCMacOSAll, AppKit, Foundation;
 
 const
-  Str_Panel_Title = 'This is the title';
-  Str_Panel_Message = 'This is the message';
+  Str_Window_Title = 'This is the title';
+  Str_Window_Message = 'This is the message';
 var
   { classes }
   pool: NSAutoreleasePool;
   MainWindow: NSWindow;
+  MainWindowView: NSView;
+  TextField: NSTextField;
   { strings }
   CFTitle, CFMessage: CFStringRef;
   { sizes }
-  MainWindowRect: NSRect;
+  MainWindowRect, TextFieldRect: NSRect;
 begin
-  {  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init]; }
+  {  Creates a AutoreleasePool for this thread. Every thread must have one }
   pool := NSAutoreleasePool.Create;
 
-  // Creates the application NSApp object
+  { Creates the application NSApp object }
   NSApp := NSApplication.sharedApplication;
 
-  // Creates a simple window
+  { Creates a simple window }
   MainWindowRect.origin.x := 300.0;
   MainWindowRect.origin.y := 300.0;
   MainWindowRect.size.width := 300.0;
@@ -47,14 +49,31 @@ begin
   MainWindow := NSWindow.initWithContentRect(MainWindowRect,
     NSTitledWindowMask or NSClosableWindowMask or NSMiniaturizableWindowMask or NSResizableWindowMask,
     NSBackingStoreBuffered, NO);
-    
+
+  { Initializes the title of the window }
+
+  CFTitle := CFStringCreateWithPascalString(nil, Str_Window_Title, kCFStringEncodingUTF8);
+  MainWindow.setTitle(CFTitle);
+
+  { Adds a NSTextField with a string }
+  TextFieldRect.origin.x := 0.0;
+  TextFieldRect.origin.y := 0.0;
+  TextFieldRect.size.width := 300.0;
+  TextFieldRect.size.height := 500.0;
+  TextField := NSTextField.initWithFrame(TextFieldRect);
+  TextField.setStringValue(CFTitle);
+  MainWindowView := NSView.CreateWithHandle(MainWindow.contentView);
+  MainWindowView.addSubview(TextField);
+
+  { Put's the window on the front z-order }
+
   MainWindow.orderFrontRegardless;
 
   { Enters main message loop }
 
   NSApp.run;
 
-  {  [pool release]; }
+  { Releases the AutoreleasePool for this thread }
   pool.Free;
 end.
 
