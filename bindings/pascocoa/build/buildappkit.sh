082c3379b646b342d2e39e59d9681aaef5498d7d
sekelsenmat <sekelsenmat@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2008-05-15 12:46:29 +0000

    Improves the PasCocoa frameworks.
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@452 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- bindings/pascocoa/build/buildappkit.sh
+++ bindings/pascocoa/build/buildappkit.sh
@@ -6,3 +6,7 @@ FRAMEWORK="/System/Library/Frameworks/AppKit.framework/Headers"
 ./objcparser -ini=$DEFAULT_INI -ini=$APPKIT_INI $FRAMEWORK/NSButton.h > ../appkit/NSButton.inc
 ./objcparser -ini=$DEFAULT_INI -ini=$APPKIT_INI $FRAMEWORK/NSWindow.h > ../appkit/NSWindow.inc
 ./objcparser -ini=$DEFAULT_INI -ini=$APPKIT_INI $FRAMEWORK/NSGraphics.h > ../appkit/NSGraphics.inc
+./objcparser -ini=$DEFAULT_INI -ini=$APPKIT_INI $FRAMEWORK/NSImage.h > ../appkit/NSImage.inc
+./objcparser -ini=$DEFAULT_INI -ini=$APPKIT_INI $FRAMEWORK/NSBitmapImageRep.h > ../appkit/NSBitmapImageRep.inc
+./objcparser -ini=$DEFAULT_INI -ini=$APPKIT_INI $FRAMEWORK/NSImageRep.h > ../appkit/NSImageRep.inc
+./objcparser -ini=$DEFAULT_INI -ini=$APPKIT_INI $FRAMEWORK/NSGraphicsContext.h > ../appkit/NSGraphicsContext.inc
