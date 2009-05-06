4df94a0b3f821c543539e410a3247ef8ef740cbe
inoussa <inoussa@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2009-05-06 16:53:48 +0000

    resource strings should go into this unit.
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@785 8e941d3f-bd1b-0410-a28a-d453659cc2b4

---
+++ wst/trunk/wst_consts.pas
@@ -0,0 +1,15 @@
+unit wst_consts;
+
+{$mode objfpc}{$H+}
+
+interface
+
+resourcestring
+  SERR_InvalidHourOffetValue       = '"%d" is not a valid hour offset value.';
+  SERR_InvalidMinuteOffetValue     = '"%d" is not a valid minute offset value.';
+  SERR_InvalidParameter            =  'Invalid parameter : "%s".';
+
+implementation
+
+end.
+
