2e522a25c6c0a639ab819c5d8fc716afa81c1340
inoussa <inoussa@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2009-05-12 10:48:21 +0000

    Add Copyright notice
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@797 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- wst/trunk/wst_consts.pas
+++ wst/trunk/wst_consts.pas
@@ -1,6 +1,18 @@
-unit wst_consts;
+{
+    This file is part of the Web Service Toolkit
+    Copyright (c) 2009 by Inoussa OUEDRAOGO
 
-{$mode objfpc}{$H+}
+    This file is provide under modified LGPL licence
+    ( the files COPYING.modifiedLGPL and COPYING.LGPL).
+
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+}
+{$INCLUDE wst_global.inc}
+
+unit wst_consts;
 
 interface
 
