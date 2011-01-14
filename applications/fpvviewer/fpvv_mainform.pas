d62432b024e0f98a10f30a65ca8bc1703c5dd1bc
sekelsenmat <sekelsenmat@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2011-01-14 14:45:47 +0000

    Basic implementation of the fpvectorial viewer
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@1447 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- applications/fpvviewer/fpvv_mainform.pas
+++ applications/fpvviewer/fpvv_mainform.pas
@@ -1,14 +1,28 @@
-unit fpvv_mainform; 
+unit fpvv_mainform;
 
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, EditBtn,
+  StdCtrls, Spin, ExtCtrls;
 
 type
-  TForm1 = class(TForm)
+
+  { TfrmFPVViewer }
+
+  TfrmFPVViewer = class(TForm)
+    btnVisualize: TButton;
+    editFileName: TFileNameEdit;
+    imageView: TImage;
+    Label2: TLabel;
+    Label3: TLabel;
+    spinStartX: TSpinEdit;
+    spinStartY: TSpinEdit;
+    spinScale: TFloatSpinEdit;
+    Label1: TLabel;
+    procedure btnVisualizeClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -16,11 +30,36 @@ type
   end; 
 
 var
-  Form1: TForm1; 
+  frmFPVViewer: TfrmFPVViewer;
 
 implementation
 
+uses
+  fpvectorial, cdrvectorialreader, svgvectorialwriter, pdfvectorialreader,
+  dxfvectorialreader,
+  fpvtocanvas;
+
 {$R *.lfm}
+
+{ TfrmFPVViewer }
+
+procedure TfrmFPVViewer.btnVisualizeClick(Sender: TObject);
+var
+  Vec: TvVectorialDocument;
+begin
+  // First check the in input
+  //if not CheckInput() then Exit;
+
+  Vec := TvVectorialDocument.Create;
+  try
+    Vec.ReadFromFile(editFileName.FileName, vfDXF);
+    imageView.Canvas.Brush.Color := clWhite;
+    imageView.Canvas.FillRect(0, 0, imageView.Width, imageView.Height);
+    DrawFPVectorialToCanvas(Vec, imageView.Canvas, spinStartX.Value, spinStartY.Value, spinScale.Value, spinScale.Value);
+  finally
+    Vec.Free;
+  end;
+end;
 
 end.
 
