b3f3cc4402225c081e3aa83e9875e40e531cb64c
denniscuflat <denniscuflat@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2011-09-10 00:58:47 +0000

    The "TappyTux Loader" interface was created, yet it's still not functional.
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@1929 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- applications/tappytux/gameconfigform.pas
+++ applications/tappytux/gameconfigform.pas
@@ -1,22 +1,41 @@
-unit gameconfigform; 
+unit gameconfigform;
 
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    btnLoad: TButton;
+    btnWordlist: TButton;
+    comboGameType: TComboBox;
+    comboSound: TComboBox;
+    comboMusic: TComboBox;
+    comboLevel: TComboBox;
+    lblGameType: TLabel;
+    listWordlist: TLabel;
+    lblSettings: TLabel;
+    lblSound: TLabel;
+    lblMusic: TLabel;
+    lblLevel: TLabel;
+    lblCredits: TLabel;
+    ltbWordlist: TListBox;
+    memoGameType: TMemo;
+    memoCredits: TMemo;
   private
     { private declarations }
   public
     { public declarations }
-  end; 
+  end;
 
 var
-  Form1: TForm1; 
+  Form1: TForm1;
 
 implementation
 
