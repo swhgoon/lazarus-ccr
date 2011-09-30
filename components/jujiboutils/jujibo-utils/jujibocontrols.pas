605a2a95140a4a131bafbaa3db0b2518954fd7e5
jujibo <jujibo@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2011-09-30 10:09:57 +0000

    Remove: Non-labeled controls. You can achieve the same functionality with labeled controls and an empty label.
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2039 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/jujiboutils/jujibo-utils/jujibocontrols.pas
+++ components/jujiboutils/jujibo-utils/jujibocontrols.pas
@@ -7,44 +7,29 @@ unit jujibocontrols;
 interface
 
 uses
-  jdbintegeredit, jdblabeledintegeredit, jdbcurrencyedit, 
-  jdblabeledcurrencyedit, jdbdateedit, jdblabeleddateedit, jcontrolutils, 
-  JIntegerEdit, JLabeledIntegerEdit, JCurrencyEdit, JLabeledCurrencyEdit, 
-  JDateEdit, JLabeledDateEdit, JDBGridControl, jdbgridutils, JDBLabeledEdit, 
-  JTimeEdit, JLabeledTimeEdit, JDBTimeEdit, JDBLabeledTimeEdit, JDateTimeEdit, 
-  JLabeledDateTimeEdit, JDBDateTimeEdit, JDBLabeledDateTimeEdit, JFloatEdit, 
-  JLabeledFloatEdit, JDBFloatEdit, JDBLabeledFloatEdit, jinputconsts, 
-  LazarusPackageIntf;
+  jdblabeledintegeredit, jdblabeledcurrencyedit, jdblabeleddateedit, 
+  jcontrolutils, JLabeledIntegerEdit, JLabeledCurrencyEdit, JLabeledDateEdit, 
+  JDBGridControl, jdbgridutils, JDBLabeledEdit, JLabeledTimeEdit, 
+  JDBLabeledTimeEdit, JLabeledDateTimeEdit, JDBLabeledDateTimeEdit, 
+  JLabeledFloatEdit, JDBLabeledFloatEdit, jinputconsts, LazarusPackageIntf;
 
 implementation
 
 procedure Register; 
 begin
-  RegisterUnit('jdbintegeredit', @jdbintegeredit.Register); 
   RegisterUnit('jdblabeledintegeredit', @jdblabeledintegeredit.Register); 
-  RegisterUnit('jdbcurrencyedit', @jdbcurrencyedit.Register); 
   RegisterUnit('jdblabeledcurrencyedit', @jdblabeledcurrencyedit.Register); 
-  RegisterUnit('jdbdateedit', @jdbdateedit.Register); 
   RegisterUnit('jdblabeleddateedit', @jdblabeleddateedit.Register); 
-  RegisterUnit('JIntegerEdit', @JIntegerEdit.Register); 
   RegisterUnit('JLabeledIntegerEdit', @JLabeledIntegerEdit.Register); 
-  RegisterUnit('JCurrencyEdit', @JCurrencyEdit.Register); 
   RegisterUnit('JLabeledCurrencyEdit', @JLabeledCurrencyEdit.Register); 
-  RegisterUnit('JDateEdit', @JDateEdit.Register); 
   RegisterUnit('JLabeledDateEdit', @JLabeledDateEdit.Register); 
   RegisterUnit('JDBGridControl', @JDBGridControl.Register); 
   RegisterUnit('JDBLabeledEdit', @JDBLabeledEdit.Register); 
-  RegisterUnit('JTimeEdit', @JTimeEdit.Register); 
   RegisterUnit('JLabeledTimeEdit', @JLabeledTimeEdit.Register); 
-  RegisterUnit('JDBTimeEdit', @JDBTimeEdit.Register); 
   RegisterUnit('JDBLabeledTimeEdit', @JDBLabeledTimeEdit.Register); 
-  RegisterUnit('JDateTimeEdit', @JDateTimeEdit.Register); 
   RegisterUnit('JLabeledDateTimeEdit', @JLabeledDateTimeEdit.Register); 
-  RegisterUnit('JDBDateTimeEdit', @JDBDateTimeEdit.Register); 
   RegisterUnit('JDBLabeledDateTimeEdit', @JDBLabeledDateTimeEdit.Register); 
-  RegisterUnit('JFloatEdit', @JFloatEdit.Register); 
   RegisterUnit('JLabeledFloatEdit', @JLabeledFloatEdit.Register); 
-  RegisterUnit('JDBFloatEdit', @JDBFloatEdit.Register); 
   RegisterUnit('JDBLabeledFloatEdit', @JDBLabeledFloatEdit.Register); 
 end; 
 
