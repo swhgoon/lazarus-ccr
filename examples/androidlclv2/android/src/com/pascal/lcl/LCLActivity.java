7452debbba4e981bea9a0d2617617b192c0ae83b
sekelsenmat <sekelsenmat@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2011-11-27 20:33:24 +0000

    Starts preparing the example for using the new jnigraphics version of the LCL
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2169 8e941d3f-bd1b-0410-a28a-d453659cc2b4

---
+++ examples/androidlclv2/android/src/com/pascal/lcl/LCLActivity.java
@@ -0,0 +1,62 @@
+package com.pascal.jnitest;
+
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.widget.*;
+import android.util.*;
+import android.graphics.*;
+import android.view.*;
+
+public class LCLActivity extends Activity 
+{
+  // Our drawing surface
+  private class LCLSurface extends SurfaceView
+  {
+    public LCLSurface(Context context)
+    {
+      super(context);
+    }
+   
+    @Override protected void onDraw(Canvas canvas)
+    {
+      Bitmap lclbitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+      LCLDrawToBitmap(getWidth(), getHeight(), lclbitmap);
+      canvas.drawBitmap(lclbitmap, 0, 0, null);
+    }
+  }    
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) 
+  {
+    super.onCreate(savedInstanceState);
+          
+//        TextView  tv = new TextView(this);
+//        tv.setText( Integer.toString(intFromJNI()) );
+//        setContentView(tv);
+    LCLSurface lclsurface = new LCLSurface(this);
+    setContentView(lclsurface);
+  }
+  
+  // JNI table of functions  
+  public native String stringFromJNI();
+  public native int intFromJNI();
+  public native int LCLDrawToBitmap(int width, int height, Bitmap bitmap);
+    
+  public long nativeCodeLoaded=0;
+     
+  static 
+  {
+    try 
+    {
+      Log.i("JNI", "Trying to load libnativetest.so");  
+      System.loadLibrary("nativetest");
+    } 
+    catch(UnsatisfiedLinkError ule) 
+    {
+      Log.e("JNI", "WARNING: Could not load libnativetest.so");
+      ule.printStackTrace();
+    }
+  }
+}
