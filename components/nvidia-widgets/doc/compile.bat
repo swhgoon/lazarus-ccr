08a5bbac2912a7b3c0638bfaa68cfa8fc074aa92
blaszijk <blaszijk@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2012-01-02 15:07:34 +0000

    Added css-file option to fpdoc, now the chm file is formatted correctly
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2217 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/nvidia-widgets/doc/compile.bat
+++ components/nvidia-widgets/doc/compile.bat
@@ -7,14 +7,14 @@ dot -Tpng nvwidgets.dot > ./images/nvwidgets.png
 del nvwidgets.dot
 
 ::create chm documentation
-fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --format=chm --output=nvwidgets.chm
+fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --css-file=fpdoc.css --format=chm --output=nvwidgets.chm
 
 ::delete old html documentation
 del *.html
-rmdir /Q /S nvwidgets
-rmdir /Q /S nvglutwidgets
-rmdir /Q /S nvglwidgets
-rmdir /Q /S nvshaderutils
+del /Q /S nvwidgets\*.html
+del /Q /S nvglutwidgets\*.html
+del /Q /S nvglwidgets\*.html
+del /Q /S nvshaderutils\*.html
 
 ::create html documentation
-fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --format=html
+fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --css-file=fpdoc.css --format=html
