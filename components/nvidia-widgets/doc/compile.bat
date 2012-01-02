4690d902a3a47081881c19b2066a98326aaaf952
blaszijk <blaszijk@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2012-01-02 14:52:12 +0000

    added html documentation moved nvwidgets.png to separate folder added css file for nicer formatting
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2216 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/nvidia-widgets/doc/compile.bat
+++ components/nvidia-widgets/doc/compile.bat
@@ -2,8 +2,19 @@ set path=%path%;H:\other\graphviz\bin
 
 fpclasschart --input=../src/nvwidgets/nvWidgets.pas --input=../src/nvwidgets/nvglwidgets.pas --input=../src/nvwidgets/nvglutwidgets.pas --input=../src/nvglutils/nvshaderutils.pas --format=graphviz  --output=nvwidgets.dot
 
-dot -Tpng nvwidgets.dot > nvwidgets.png
+dot -Tpng nvwidgets.dot > ./images/nvwidgets.png
 
 del nvwidgets.dot
 
+::create chm documentation
 fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --format=chm --output=nvwidgets.chm
+
+::delete old html documentation
+del *.html
+rmdir /Q /S nvwidgets
+rmdir /Q /S nvglutwidgets
+rmdir /Q /S nvglwidgets
+rmdir /Q /S nvshaderutils
+
+::create html documentation
+fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --format=html
