9d7d1a403f251e2899fe323a5b2776d547dbb198
blaszijk <blaszijk@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2012-01-02 11:08:11 +0000

    modified script so it creates the classchart more documentation added
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2212 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/nvidia-widgets/doc/compile.bat
+++ components/nvidia-widgets/doc/compile.bat
@@ -1,1 +1,9 @@
-fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --format=chm  --output=nvwidgets.chm
+set path=%path%;H:\other\graphviz\bin
+
+fpclasschart --input=../src/nvwidgets/nvWidgets.pas --input=../src/nvwidgets/nvglwidgets.pas --input=../src/nvwidgets/nvglutwidgets.pas --input=../src/nvglutils/nvshaderutils.pas --format=graphviz  --output=nvwidgets.dot
+
+dot -Tpng nvwidgets.dot > nvwidgets.png
+
+del nvwidgets.dot
+
+fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --format=chm --output=nvwidgets.chm
