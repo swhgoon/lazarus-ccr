1a65aa2ff7d85f5b52eb6aaec9c27be9d642403e
blaszijk <blaszijk@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2012-01-03 19:24:36 +0000

    added image-url parameter to fpdoc added glfreetype documentation recreated chm documentation (with embedded images)
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2223 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/nvidia-widgets/doc/compile.bat
+++ components/nvidia-widgets/doc/compile.bat
@@ -10,7 +10,11 @@ del nvwidgets.dot
 del *.chm
 
 ::create chm documentation
-fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --descr=glfreetype.xml --input=../src/gl/glfreetype.pas --css-file=fpdoc.css --format=chm --output=nvwidgets.chm
+fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --descr=glfreetype.xml --input=../src/gl/glfreetype.pas --css-file=fpdoc.css --image-url=images/ --format=chm --output=nvwidgets.chm
+
+::decompile the chm file (for debugging)
+::del /Q /S html\*
+::hh -decompile html nvwidgets.chm
 
 ::delete old html documentation
 del *.html
@@ -20,4 +24,4 @@ del /Q /S nvglwidgets\*.html
 del /Q /S nvshaderutils\*.html
 
 ::create html documentation
-fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --descr=glfreetype.xml --input=../src/gl/glfreetype.pas --format=html
+fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --descr=glfreetype.xml --input=../src/gl/glfreetype.pas --image-url=images/ --format=html
