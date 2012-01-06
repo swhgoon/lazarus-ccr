0ebead61c2e938367f43f90de524e2bf84175b26
blaszijk <blaszijk@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2012-01-06 21:45:31 +0000

    fixed source paths, added nvbasepath unit documentation added relative paths to crop_images.bat
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2238 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/nvidia-widgets/doc/compile.bat
+++ components/nvidia-widgets/doc/compile.bat
@@ -1,6 +1,6 @@
 set path=%path%;H:\other\graphviz\bin
 
-fpclasschart --input=../src/nvwidgets/nvWidgets.pas --input=../src/nvwidgets/nvglwidgets.pas --input=../src/nvwidgets/nvglutwidgets.pas --input=../src/nvglutils/nvshaderutils.pas --input=../src/gl/glfreetype.pas --format=graphviz  --output=nvwidgets.dot
+fpclasschart --input=../src/nvwidgets.pas --input=../src/gl/nvglwidgets.pas --input=../src/glut/nvglutwidgets.pas --input=../src/gl/nvshaderutils.pas --input=../src/gl/glfreetype.pas --input=../src/nvbasefont.pas --format=graphviz  --output=nvwidgets.dot
 
 dot -Tpng nvwidgets.dot > ./images/nvwidgets.png
 
@@ -10,7 +10,7 @@ del nvwidgets.dot
 del *.chm
 
 ::create chm documentation
-fpdoc --package=nvidia-widgets --auto-index --auto-toc --make-searchable --footer-date="mmm dd yyyy" --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --descr=glfreetype.xml --input=../src/gl/glfreetype.pas --css-file=fpdoc.css --image-url=images/ --format=chm --output=nvwidgets.chm
+fpdoc --package=nvidia-widgets --auto-index --auto-toc --make-searchable --footer-date="mmm dd yyyy" --descr=nvwidgets.xml --input=../src/nvwidgets.pas --descr=nvglwidgets.xml --input=../src/gl/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/glut/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/gl/nvshaderutils.pas --descr=glfreetype.xml --input=../src/gl/glfreetype.pas --descr=nvbasefont.xml --input=../src/nvbasefont.pas --css-file=fpdoc.css --image-url=images/ --format=chm --output=nvwidgets.chm
 
 ::decompile the chm file (for debugging)
 ::del /Q /S html\*
@@ -25,4 +25,4 @@ del /Q /S nvshaderutils\*.html
 
 ::create html documentation
 ::once documentation settles add --footer-date="mmm dd yyyy" 
-fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets/nvWidgets.pas --descr=nvglwidgets.xml --input=../src/nvwidgets/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/nvwidgets/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/nvglutils/nvshaderutils.pas --descr=glfreetype.xml --input=../src/gl/glfreetype.pas --image-url=images/ --format=html
+fpdoc --package=nvidia-widgets --descr=nvwidgets.xml --input=../src/nvwidgets.pas --descr=nvglwidgets.xml --input=../src/gl/nvglwidgets.pas --descr=nvglutwidgets.xml --input=../src/glut/nvglutwidgets.pas --descr=nvshaderutils.xml --input=../src/gl/nvshaderutils.pas --descr=glfreetype.xml --input=../src/gl/glfreetype.pas --descr=nvbasefont.xml --input=../src/nvbasefont.pas --image-url=images/ --format=html
--- components/nvidia-widgets/doc/images/crop_images.bat
+++ components/nvidia-widgets/doc/images/crop_images.bat
@@ -3,21 +3,23 @@
 ::a separate file
 ::the crop application is located in ./utils
 
-crop widget_test.png 7 32 152 79 label.png
-crop widget_test.png 7 79 182 109 button.png
-crop widget_test.png 7 121 406 180 checkbutton.bmp
-crop widget_test.png 7 190 394 251 radiobutton.bmp
-crop widget_test.png 7 249 117 279 horizontal_slider.png
-crop widget_test.png 7 279 118 348 listbox.png
-crop widget_test.png 116 279 255 348 combobox.png
-crop widget_test.png 7 348 235 383 line_edit.png
-crop widget_test.png 7 383 262 458 panel.png
-crop widget_test.png 7 458 262 537 frame.png
-crop widget_test.png 4 537 75 618 listitem.png
-crop widget_test.png 4 615 114 724 textureview.bmp
+..\..\utils\crop widget_test.png 4 29 152 81 label.png
+..\..\utils\crop widget_test.png 7 79 182 111 button.png
+..\..\utils\crop widget_test.png 7 118 406 180 checkbutton.bmp
+..\..\utils\crop widget_test.png 7 190 394 251 radiobutton.bmp
+..\..\utils\crop widget_test.png 7 253 117 279 horizontal_slider.png
+..\..\utils\crop widget_test.png 7 279 120 350 listbox.png
+..\..\utils\crop widget_test.png 116 279 255 348 combobox.png
+..\..\utils\crop widget_test.png 7 353 237 385 line_edit.png
+..\..\utils\crop widget_test.png 7 383 264 460 panel.png
+..\..\utils\crop widget_test.png 7 468 262 539 frame.png
+..\..\utils\crop widget_test.png 4 537 77 620 listitem.png
+..\..\utils\crop widget_test.png 4 615 114 724 textureview.bmp
 
-del checkbutton.bmp
-del radiobutton.bmp
-del textureview.bmp
+del checkbutton.png
+del radiobutton.png
+del textureview.png
 
-ren *.bmp *.png
+ren checkbutton.bmp checkbutton.png
+ren radiobutton.bmp radiobutton.png
+ren textureview.bmp textureview.png
