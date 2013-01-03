e191a469b5f766d6d200d01a200e1e14b09852f8
jujibo <jujibo@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2013-01-03 15:50:39 +0000

    Added:  TJDbEnumCombo, like TDbComboBox but, uses itemindex instead of text
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2616 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/jujiboutils/jujiboutils.pas
+++ components/jujiboutils/jujiboutils.pas
@@ -2,7 +2,7 @@
   This source is only used to compile and install the package.
  }
 
-unit jujiboutils; 
+unit jujiboutils;
 
 interface
 
@@ -11,29 +11,30 @@ uses
   jdblabeledcurrencyedit, jdblabeleddateedit, jcontrolutils, 
   JLabeledIntegerEdit, JLabeledFloatEdit, JLabeledCurrencyEdit, 
   JLabeledDateEdit, jdbgridutils, JLabeledTimeEdit, JDBLabeledTimeEdit, 
-  JLabeledDateTimeEdit, JDBLabeledDateTimeEdit, jinputconsts, 
+  JLabeledDateTimeEdit, JDBLabeledDateTimeEdit, jinputconsts, JDbEnumCombo, 
   LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-  RegisterUnit('JDBGridControl', @JDBGridControl.Register); 
-  RegisterUnit('JDBLabeledEdit', @JDBLabeledEdit.Register); 
-  RegisterUnit('jdblabeledintegeredit', @jdblabeledintegeredit.Register); 
-  RegisterUnit('JDBLabeledFloatEdit', @JDBLabeledFloatEdit.Register); 
-  RegisterUnit('jdblabeledcurrencyedit', @jdblabeledcurrencyedit.Register); 
-  RegisterUnit('jdblabeleddateedit', @jdblabeleddateedit.Register); 
-  RegisterUnit('JLabeledIntegerEdit', @JLabeledIntegerEdit.Register); 
-  RegisterUnit('JLabeledFloatEdit', @JLabeledFloatEdit.Register); 
-  RegisterUnit('JLabeledCurrencyEdit', @JLabeledCurrencyEdit.Register); 
-  RegisterUnit('JLabeledDateEdit', @JLabeledDateEdit.Register); 
-  RegisterUnit('JLabeledTimeEdit', @JLabeledTimeEdit.Register); 
-  RegisterUnit('JDBLabeledTimeEdit', @JDBLabeledTimeEdit.Register); 
-  RegisterUnit('JLabeledDateTimeEdit', @JLabeledDateTimeEdit.Register); 
-  RegisterUnit('JDBLabeledDateTimeEdit', @JDBLabeledDateTimeEdit.Register); 
-end; 
+  RegisterUnit('JDBGridControl', @JDBGridControl.Register);
+  RegisterUnit('JDBLabeledEdit', @JDBLabeledEdit.Register);
+  RegisterUnit('jdblabeledintegeredit', @jdblabeledintegeredit.Register);
+  RegisterUnit('JDBLabeledFloatEdit', @JDBLabeledFloatEdit.Register);
+  RegisterUnit('jdblabeledcurrencyedit', @jdblabeledcurrencyedit.Register);
+  RegisterUnit('jdblabeleddateedit', @jdblabeleddateedit.Register);
+  RegisterUnit('JLabeledIntegerEdit', @JLabeledIntegerEdit.Register);
+  RegisterUnit('JLabeledFloatEdit', @JLabeledFloatEdit.Register);
+  RegisterUnit('JLabeledCurrencyEdit', @JLabeledCurrencyEdit.Register);
+  RegisterUnit('JLabeledDateEdit', @JLabeledDateEdit.Register);
+  RegisterUnit('JLabeledTimeEdit', @JLabeledTimeEdit.Register);
+  RegisterUnit('JDBLabeledTimeEdit', @JDBLabeledTimeEdit.Register);
+  RegisterUnit('JLabeledDateTimeEdit', @JLabeledDateTimeEdit.Register);
+  RegisterUnit('JDBLabeledDateTimeEdit', @JDBLabeledDateTimeEdit.Register);
+  RegisterUnit('JDbEnumCombo', @JDbEnumCombo.Register);
+end;
 
 initialization
-  RegisterPackage('jujiboutils', @Register); 
+  RegisterPackage('jujiboutils', @Register);
 end.
