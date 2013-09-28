6e06cfea8f26efe7230ba7ee80ac08b6a7e9ced0
alexs75 <alexs75@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2013-09-28 16:11:28 +0000

    RxDateEdit - fix button icon
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2797 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- components/rx/images/componet_img/all_component_res.sh
+++ components/rx/images/componet_img/all_component_res.sh
@@ -1,5 +1,10 @@
 rm tooledit.lrs
 rm rxdbgrid.lrs
+rm tooledit.res
+rm rxdbgrid.res
 
-../../../../tools\lazres.exe tooledit.lrs picDateEdit.bmp
-../../../../tools\lazres.exe rxdbgrid.lrs rx_markerdown.xpm rx_markerup.xpm
+/usr/local/share/lazarus/tools/lazres tooledit.res picDateEdit.png
+/usr/local/share/lazarus/tools/lazres rxdbgrid.res rx_markerdown.png rx_markerup.png
+
+#../../../../tools\lazres.exe tooledit.lrs picDateEdit.bmp
+#../../../../tools\lazres.exe rxdbgrid.lrs rx_markerdown.xpm rx_markerup.xpm
