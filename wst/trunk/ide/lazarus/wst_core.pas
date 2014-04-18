cc9eeb3df0340bb1359bbe91e01aa815c1017fcc
inoussa <inoussa@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2014-04-18 14:47:54 +0000

    Package update for logger.
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@2948 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- wst/trunk/ide/lazarus/wst_core.pas
+++ wst/trunk/ide/lazarus/wst_core.pas
@@ -1,4 +1,4 @@
-{ This file was automatically created by Lazarus. do not edit!
+{ This file was automatically created by Lazarus. Do not edit!
   This source is only used to compile and install the package.
  }
 
@@ -7,7 +7,7 @@ unit wst_core;
 interface
 
 uses
-  xmlrpc_formatter, base_binary_formatter, base_json_formatter, 
+    xmlrpc_formatter, base_binary_formatter, base_json_formatter, 
   base_service_intf, base_soap_formatter, base_xmlrpc_formatter, basex_encode, 
   binary_formatter, binary_streamer, config_objects, imp_utils, 
   json_formatter, library_base_intf, library_imp_utils, library_protocol, 
@@ -17,7 +17,7 @@ uses
   server_listener, server_service_imputils, server_service_intf, 
   server_service_json, server_service_soap, server_service_xmlrpc, 
   service_intf, soap_formatter, wst_fpc_xml, wst_initialization, 
-  wst_resources_imp, wst_types;
+  wst_resources_imp, wst_types, file_logger_extension, server_filter_extension;
 
 implementation
 
