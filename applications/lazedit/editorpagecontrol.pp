4cf1f36f96e6f7fd1a0818d3f88e7f896431b232
lazarus-bart <lazarus-bart@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2014-06-29 12:03:21 +0000

    LazEdit: when calling FileGetAttrUTF8 check for LongInt(feInvalidHandle) instead of feInvalidHandle.
    git-svn-id: http://svn.code.sf.net/p/lazarus-ccr/svn@3251 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- applications/lazedit/editorpagecontrol.pp
+++ applications/lazedit/editorpagecontrol.pp
@@ -688,7 +688,7 @@ begin
     //because it uses FILE_ATTRIBUTE_NORMAL
     //see http://msdn.microsoft.com/en-us/library/windows/desktop/aa363858%28v=vs.85%29.aspx
     Attr := FileGetAttrUTF8(Utf8Fn);
-    if (Attr = feInvalidHandle) then Attr := 0;
+    if (Attr = LongInt(feInvalidHandle)) then Attr := 0;
     if ((Attr and faHidden) = faHidden) then FileSetAttrUtf8(Utf8Fn, Attr and (not faHidden));
     {$endif}
     Lines.SaveToFile(Utf8Fn);
